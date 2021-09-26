710fd2240c76b8f91ba5199a4bab91ef1322e637
Sergey <sergekotov95@gmail.com>
2021-09-27 00:42:18 +0300

    Getting char info from server
    Added a possibility to find random char from server, written functions to transform houses and books

---
+++ src/services/gotService.js
@@ -0,0 +1,75 @@
+export default class GotService {
+    constructor() {
+        this._apiBase = 'https://anapioficeandfire.com/api'; 
+    }
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` + 
+            `, received ${res.status}`);
+        }
+        return await res.json(); 
+    }
+
+    async getAllCharacters() {
+        const res = await this.getResource(`/characters?page=5&pageSize=10`);
+        return res.map(this._trasformCharacter);
+    }
+
+    async getCharacter(id){
+        const character = await this.getResource(`/characters/${id}`); 
+        return this._trasformCharacter(character); 
+    }
+
+    async getallHouses() {
+        const res = await this.getResource(`/houses/`);
+        return res.map(this._transformHouse);
+    }
+
+    async getHouse(id) {
+        const house = await this.getResource(`/houses/${id}`);
+        return this._transformHouse(house); 
+    }
+
+    async getAllBooks() {
+        const res = await this.getResource(`/books/`);
+        res.map(this._transformBook);
+    }
+
+    async getBook(id) {
+        const book = await this.getResource(`/books/${id}`); 
+        return this._transformBook(book); 
+    }
+
+    _trasformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender, 
+            born: char.born, 
+            died: char.died, 
+            culture: char.culture
+        }
+    }
+
+    _transformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles, 
+            overlord: house.overlord, 
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publisher: book.publisher,
+            released: book.released 
+        }
+    }
+}
